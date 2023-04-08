8b50bdbeb1cf5c7abc7584990a24d1349700620e
Hernan Pereira <pereirahernan@outlook.com>
2023-04-08 18:27:27 -0500

    [ADD] Local strategy & login with sessions

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 
 import { AuthService } from '../services/auth.service';
 import { LocalAuthGuard } from 'src/common/guards/local-auth.guard';
